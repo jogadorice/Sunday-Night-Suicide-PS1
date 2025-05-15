1b4b84d9edfbab9f61e94ecad0ad1178e62631eb
mrmeep64 <tsp.bustamante.perez@gmail.com>
2025-05-15 18:57:09 -0400

    hi

--- assets/data/characters/bf-pixel.lua
+++
@@ -1,6 +0,0 @@
-function onGameOver(event)
-	event.characterName = 'bf-pixel-dead'
-	event.deathSoundName = 'gameplay/fnf_loss_sfx-pixel'
-	event.loopSoundName = 'gameOver-pixel'
-	event.endSoundName = 'gameOverend-pixel'
-end
